fe4bb4230f0442fd909ece2ce562b7c08919b93d
Owen Richards <cabbagedude88@yahoo.co.uk>
2017-12-10 19:21:28 +0000

    Day 2 completed

---
+++ day4/part2.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+fs.readFile('day4/input.txt', 'utf8', (err,input) => {
+
+    if (err) return console.log(err);
+
+    let lines = input.match(/(.*)\n?/g) || [];
+
+    let filtered = lines.filter(line => {
+        if( !line ) return false;
+        let words = line.replace('\n', '').split(" ").map(word => word.split("").sort().join("")),
+            unique = words.filter((word, i, arr) => arr.indexOf(word) === i);
+        return words.length === unique.length;
+    });
+
+    console.log( filtered.length )
+
+});
